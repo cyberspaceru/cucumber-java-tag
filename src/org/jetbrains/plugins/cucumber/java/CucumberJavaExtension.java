c1dd0c5737174074c30cb3a2a5187d7c0f25fdeb
cyberspaceru <cyberspaceroot@gmail.com>
2017-07-30 12:07:01 +0600

    Add refernces to pageEntry and actionTitle

--- src/org/jetbrains/plugins/cucumber/java/CucumberJavaExtension.java
+++ src/org/jetbrains/plugins/cucumber/java/CucumberJavaExtension.java
@@ -5,7 +5,6 @@ import com.intellij.openapi.module.Module;
 import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiMethod;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.searches.AnnotatedElementsSearch;
 import com.intellij.util.Query;
@@ -16,46 +15,52 @@ import org.jetbrains.plugins.cucumber.StepDefinitionCreator;
 import org.jetbrains.plugins.cucumber.java.steps.JavaStepDefinition;
 import org.jetbrains.plugins.cucumber.java.steps.JavaStepDefinitionCreator;
 import org.jetbrains.plugins.cucumber.steps.AbstractStepDefinition;
+import ru.sbtqa.plugins.cucumber.util.TagProject;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class CucumberJavaExtension extends AbstractCucumberJavaExtension {
-  public static final String CUCUMBER_RUNTIME_JAVA_STEP_DEF_ANNOTATION = "cucumber.runtime.java.StepDefAnnotation";
+    public static final String CUCUMBER_RUNTIME_JAVA_STEP_DEF_ANNOTATION = "cucumber.runtime.java.StepDefAnnotation";
 
-  @NotNull
-  @Override
-  public BDDFrameworkType getStepFileType() {
-    return new BDDFrameworkType(JavaFileType.INSTANCE);
-  }
-
-  @NotNull
-  @Override
-  public StepDefinitionCreator getStepDefinitionCreator() {
-    return new JavaStepDefinitionCreator();
-  }
-
-  @Override
-  public List<AbstractStepDefinition> loadStepsFor(@Nullable PsiFile featureFile, @NotNull Module module) {
-    final GlobalSearchScope dependenciesScope = module.getModuleWithDependenciesAndLibrariesScope(true);
-
-    PsiClass stepDefAnnotationClass = JavaPsiFacade.getInstance(module.getProject()).findClass(CUCUMBER_RUNTIME_JAVA_STEP_DEF_ANNOTATION,
-                                                                                               dependenciesScope);
-    if (stepDefAnnotationClass == null) {
-      return Collections.emptyList();
+    @NotNull
+    @Override
+    public BDDFrameworkType getStepFileType() {
+        return new BDDFrameworkType(JavaFileType.INSTANCE);
     }
 
-    final List<AbstractStepDefinition> result = new ArrayList<>();
-    final Query<PsiClass> stepDefAnnotations = AnnotatedElementsSearch.searchPsiClasses(stepDefAnnotationClass, dependenciesScope);
-    for (PsiClass annotationClass : stepDefAnnotations) {
-      if (annotationClass.isAnnotationType()) {
-        final Query<PsiMethod> javaStepDefinitions = AnnotatedElementsSearch.searchPsiMethods(annotationClass, dependenciesScope);
-        for (PsiMethod stepDefMethod : javaStepDefinitions) {
-          result.add(new JavaStepDefinition(stepDefMethod));
+    @NotNull
+    @Override
+    public StepDefinitionCreator getStepDefinitionCreator() {
+        return new JavaStepDefinitionCreator();
+    }
+
+    @Override
+    public List<AbstractStepDefinition> loadStepsFor(@Nullable PsiFile featureFile, @NotNull Module module) {
+        final GlobalSearchScope dependenciesScope = module.getModuleWithDependenciesAndLibrariesScope(true);
+
+        final List<AbstractStepDefinition> result = new ArrayList<>();
+        result.addAll(loadAnnotatedBy(CUCUMBER_RUNTIME_JAVA_STEP_DEF_ANNOTATION, dependenciesScope, module));
+        result.addAll(loadAnnotatedBy(TagProject.PAGE_ENTRY_ANNOTATION_QUALIFIED_NAME, dependenciesScope, module));
+        result.addAll(loadAnnotatedBy(TagProject.ACTION_TITLE_ANNOTATION_QUALIFIED_NAME, dependenciesScope, module));
+        return result;
+    }
+
+    private List<AbstractStepDefinition> loadAnnotatedBy(String annotationQualifiedName, GlobalSearchScope dependenciesScope, Module module) {
+        PsiClass psiAnnotationClass = JavaPsiFacade.getInstance(module.getProject()).findClass(annotationQualifiedName, dependenciesScope);
+        if (psiAnnotationClass == null)
+            return Collections.emptyList();
+
+        final List<AbstractStepDefinition> result = new ArrayList<>();
+        final Query<PsiClass> defAnnotations = AnnotatedElementsSearch.searchPsiClasses(psiAnnotationClass, dependenciesScope);
+        for (PsiClass annotationClassInstance : defAnnotations) {
+            if (annotationClassInstance.isAnnotationType())
+                AnnotatedElementsSearch.searchPsiMethods(annotationClassInstance, dependenciesScope).findAll().forEach(x -> result.add(new JavaStepDefinition(x)));
+            else if (!annotationQualifiedName.equals(CUCUMBER_RUNTIME_JAVA_STEP_DEF_ANNOTATION))
+                result.add(new JavaStepDefinition(annotationClassInstance));
         }
-      }
+        return result;
     }
-    return result;
-  }
+
 }
